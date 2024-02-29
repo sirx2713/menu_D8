8af1a056a22cfc7334dc1af96a06add7c36090a7
Sirx <bradx2713@gmail.com>
2024-02-29 07:11:03 +0200

    Initial Commit

---
+++ menu_D8/menu_D8App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D8App.swift
+//  menu_D8
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct menu_D8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
